67686c9f98c8a0105fd19f60fbdfcb85e685f696
sakshamsondhi <sakshamsondhi1996@gmail.com>
2023-04-19 09:11:03 +0530

    testing various scenarios on HTTP server

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
-app.get('/', function (req, res)  {
+app.get('/route1', function (req, res)  {
   res.send('Hello World!')
+})
+
+app.get('/route2', function (req, res)  {
+  res.send('Hello World from Route2!')
+})
+
+app.get('/chat', function (req, res)  {
+  res.send('<html><body><h1 style="color : red">Chat</h1></body></html>')
 })
 
 app.listen(port, function() {
